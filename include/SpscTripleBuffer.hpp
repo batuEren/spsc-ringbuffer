129a05b98add6799daa1ad457d8cc8999e7085d0
Batu Eren <batu.eren2004@gmail.com>
2026-03-31 02:19:40 +0200

    triple buffer structure

--- include/SpscTripleBuffer.hpp
+++ include/SpscTripleBuffer.hpp
@@ -5,3 +5,25 @@
 #include <type_traits>
 #include <memory>
 #include <stdexcept>
+
+
+template<typename T, typename Alloc = std::allocator<T>>
+class SpscTripleBuffer : private Alloc {
+public:
+	using value_type = T;
+	using allocator_type = Alloc;
+	using allocator_traits = std::allocator_traits<Alloc>;
+	using size_type = typename allocator_traits::size_type
+
+	static_assert(std::is_trivially_copyable_v<T>,
+			"SpscTripleBuffer requires trivially copyable T");
+
+	explicit SpscTripleBuffer() { // constructor
+
+	}
+
+	~SpscTripleBuffer() { // destructor
+
+	}
+
+};
